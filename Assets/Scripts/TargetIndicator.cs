dff117565dcefdaf1617e155179dba5089af72bf
Alibek Olimov <98762357+OlimovAlibek@users.noreply.github.com>
2024-12-04 15:22:18 +0500

    Update TargetIndicator.cs
    Target Indicator improving with adding comments

--- Assets/Scripts/TargetIndicator.cs
+++ Assets/Scripts/TargetIndicator.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 
 public class TargetIndicator : MonoBehaviour
 {
+    // The target object the indicator will point to
     [SerializeField]
-    public GameObject Target=null;
+    public GameObject Target = null;
 
-    void Update(){
-        if (Target!=null){
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if a target is assigned
+        if (Target != null)
+        {
+            // Calculate the direction vector from the indicator to the target
             var dir = Target.transform.position - transform.position;
+
+            // Calculate the angle (in degrees) of the direction vector
+            // Mathf.Atan2 gives the angle in radians; convert it to degrees
             var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
 
+            // Rotate the indicator to point at the target using the calculated angle
             transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         }
     }
